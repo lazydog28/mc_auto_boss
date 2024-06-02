8bd478f7253e36566e48319fb428216f1cb93cff
SuperLazyDog <yun981128@gmail.com>
2024-06-02 17:41:28 +0800

    0.0.4 鼠标位置检测每0.01秒检测一次

--- background/mouse_reset.py
+++ background/mouse_reset.py
@@ -5,6 +5,8 @@
 @time: 2024/6/2 下午4:02
 @author SuperLazyDog
 """
+import time
+
 from pynput.mouse import Controller
 import math
 from datetime import datetime
@@ -16,13 +18,10 @@ def mouse_reset(e: Event):
     logger_msg("鼠标重置进程启动成功")
     mouse = Controller()
     last_position = mouse.position
-    last_time = datetime.now()
     while True:
+        time.sleep(0.01)  # 0.01秒检测一次
         if e.is_set():
             break
-        if (datetime.now() - last_time).total_seconds() < 0.01:
-            continue
-        last_time = datetime.now()
         current_position = mouse.position
         distance = math.sqrt(
             (current_position[0] - last_position[0]) ** 2
