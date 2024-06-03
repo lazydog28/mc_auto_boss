a35902a983bee2262b14c55e816caa8c193eb0a2
SuperLazyDog <yun981128@gmail.com>
2024-06-03 19:41:39 +0800

    更新一键启动脚本

--- background/main.py
+++ background/main.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import check
 from utils import screenshot, ocr, search_text
 import time
--- 一键启动.bat
+++ 一键启动.bat
@@ -1,6 +1,10 @@
 @echo off
 chcp 65001
+rem 进入当前文件夹
+cd /d %~dp0
 
+@REM 输出当前目录
+echo 当前目录：%CD%
 
 @REM 判断当前是否以管理员权限运行，如果不是则提示并退出
 net session >nul 2>nul
@@ -27,9 +31,8 @@ if "%hasChinese%" == "true" (
 )
 
 @REM 判断 py310 文件夹是否存在 如果不存在则下载 https://cloudreve.caiyun.fun/f/x2ux/py310.zip 并解压到 py310 文件夹
-if not exist py310 (
+if not exist py310\ (
     echo 未找到 Python 3.10 环境，正在下载并解压...
-@REM     curl -k -L https://hermes981128.oss-cn-shanghai.aliyuncs.com/py310.zip -o py310.zip
     curl -k -L https://cloudreve.caiyun.fun/f/x2ux/py310.zip -o py310.zip
     @REM     判断 是否存在 py310 文件夹，如果不存在则解压
     if not exist py310 (
@@ -37,17 +40,14 @@ if not exist py310 (
         mkdir py310
     )
     tar  -xf py310.zip -C py310
+    (echo python310.zip) > py310/python310._pth
+    (echo .) >> py310/python310._pth
+    (echo import site) >> py310/python310._pth
     del py310.zip
 ) else (
     echo Python 3.10 环境已存在
 )
 
-
-rem 管理员启动
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-chcp 65001
-rem 进入当前文件夹
-cd /d %~dp0
 @REM 添加临时环境变量 python310 目录
 set PATH=%~dp0\py310;%PATH%
 @REM 判断是否有pip, 如果没有则 运行 python 执行 get-pip.py
