35c7c2d3b5c4cc1743ea9da9fba3aa0c499ea272
ArcS17 <94548426+ArcS17@users.noreply.github.com>
2024-07-05 03:43:14 +0800

    feat:扩展所适配分辨率并增添提醒,定时重启,截取失败重新拉起,声骸识别失败提醒; fix:优化崩溃重启逻辑,log读取bug合并#196

--- background/auto_yolo_switch.py
+++ background/auto_yolo_switch.py
@@ -47,8 +47,7 @@ def model_boss_yolo(bossName:str):
     #  todo...待训练其他的BOSS
 
     # utils.py 222行调用当前方法
-    elif (bossName == "鸣钟之龟" or bossName=="无冠者" or bossName=="朔雷之鳞" or bossName=="云闪之鳞" or bossName=="燎照之骑" or bossName=="飞廉之猩" or  bossName == "袁声鸷" or bossName == "无常凶鹭" or bossName == "辉萤军势" or bossName == "聚械机偶"):
-
+    elif (bossName == "鸣钟之龟" or bossName=="无冠者" or bossName=="朔雷之鳞" or bossName=="云闪之鳞" or bossName=="燎照之骑" or bossName=="飞廉之猩" or  bossName == "袁声鸷" or bossName == "无常凶鹭" or bossName == "辉萤军势" or bossName == "聚械机偶") and config.ModelName!=defaultModelName:
 
         user_default_model()           #  其他Boss，使用默认的yolo模型
                   
