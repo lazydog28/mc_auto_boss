a167bf80fe00c33332ef8829764b5f26150713a6
wakening <32231663+wakening@users.noreply.github.com>
2025-03-29 23:20:23 +0800

    v2.2.0, 支持新boss，新套装, 详见readme更新日志

--- background/main.py
+++ background/main.py
@@ -354,3 +354,4 @@ if __name__ == "__main__":
     with Listener(on_press=on_press) as listener:
         listener.join()
     logger("结束运行")
+    logger("全新GUI预览版，欢迎体验：https://github.com/wakening/WutheringWavesAssistant")
